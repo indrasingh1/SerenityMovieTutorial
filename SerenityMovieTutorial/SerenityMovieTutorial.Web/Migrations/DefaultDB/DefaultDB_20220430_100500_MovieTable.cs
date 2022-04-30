eb6ea3dbc64008fc425bb82effd6beef444601c4
isingh <indra.singh@quicken.com>
2022-04-30 12:32:02 +0530

    Add Movie table

---
+++ SerenityMovieTutorial/SerenityMovieTutorial.Web/Migrations/DefaultDB/DefaultDB_20220430_100500_MovieTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using System;
+
+namespace SerenityProject.Migrations.DefaultDB
+{
+    [Migration(20220430_100500)]
+    public class DefaultDB_20220430_100500_MovieTable : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Schema("mov");
+
+            Create.Table("Movie").InSchema("mov")
+                .WithColumn("MovieId").AsInt32()
+                    .Identity().PrimaryKey().NotNullable()
+                .WithColumn("Title").AsString(200).NotNullable()
+                .WithColumn("Description").AsString(1000).Nullable()
+                .WithColumn("Storyline").AsString(Int32.MaxValue).Nullable()
+                .WithColumn("Year").AsInt32().Nullable()
+                .WithColumn("ReleaseDate").AsDateTime().Nullable()
+                .WithColumn("Runtime").AsInt32().Nullable();
+        }
+    }
+}
